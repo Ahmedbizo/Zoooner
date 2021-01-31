53eb9ebf4b4f11e06f4e3df49ff790c4d30a722b
Ahmed Bizo <ahmedmajedb@hotmail.com>
2021-02-01 00:16:29 +0100

    greated product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,25 +22,7 @@ function App() {
 
             data.products.map ( product =>(
 
-              <div key={product._id} className="card">
-            <a href={'/product/$ {product._id} '}>
-              <img className="medium" src= {product.image} alt= {product.name} />
-            </a>
-            <div className="card-body">
-            <a href={'/product/$ {product._id} '}>
-                <h2>{product.name}</h2>
-              </a>  
-              <div className="rating">
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star-o"></i> </span>
-                <span> <i className="fa fa-star-o"></i> </span>
-                <span> <i className="fa fa-star-o"></i> </span>
-              </div>
-              <div className="price">$ {product.price} </div>
-            </div>
-          </div>
-
+             <product key={product._id} product={product}></product>
 
             ))
 
